6211f4f96cb476a24550a0ae38629606fedcaeb1
Evan Silva González <a20evansg@ateis.local>
2024-03-12 14:22:42 +0100

    build(Cotxox):Creating Cotxox Basic Project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+]
+-app/src/main/java/edu/badpals/cotxox/App.java,1\e\1e653a93ccd4459dfa26a61a9755dd11c664f487
+a
+1app/src/test/java/edu/badpals/cotxox/AppTest.java,f\8\f8e88b28cd8f888eaf678f220ed4639b94b19901
+`
+0app/src/main/java/edu/badpals/cotxox/Cotxox.java,9\d\9dbca614c0f5bbcef615d3afae5cefb6a8974d88
+i
+9app/src/main/java/edu/badpals/cotxox/carrera/Carrera.java,d\0\d0cc38c7ee862ed147d5fab991067617d9e2babc
---
+++ app/src/main/java/edu/badpals/cotxox/carrera/Carrera.java
@@ -0,0 +1,34 @@
+package edu.badpals.cotxox.carrera;
+
+public class Carrera {
+
+
+    private String tarjetaCredito = "";
+    private String origen = "";
+    private String destino = "";
+    private Double Distancia = 0.0;
+    private int tiempoEsperado = 0;
+    private int tiempoCarrera = 0;
+    private Double costeTotal = 0.0;
+    private int propina = 0;
+    Private Conductor condductor = null;
+
+    public Carrera(String carrera) {
+        this.tarjetaCredito = tarjetaCredito;
+        this.origen = origen;
+        this.destino = destino;
+        this.Distancia = Distancia;
+        this.tiempoEsperado = tiempoEsperado;
+        this.tiempoCarrera = tiempoCarrera;
+        this.costeTotal = costeTotal;
+        this.propina = propina;
+    }
+
+    public String getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+
+    public String tarje() {
+        return origen;
+    }
+}
